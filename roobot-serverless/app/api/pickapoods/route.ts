1babca49d20fe95f9228c4ef2566daab1da4cf97
ryanchua00 <ryanchuahj@gmail.com>
2024-07-12 15:44:19 +0800

    restart vercel functions

--- roobot-serverless/app/api/pickapoods/route.ts
+++ roobot-serverless/app/api/pickapoods/route.ts
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
-  return new Response(`Hello, Received ${request}`);
+  return new Response(`Hello, received ${request}`);
 }
