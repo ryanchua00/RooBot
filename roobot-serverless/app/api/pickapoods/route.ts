3af6be70f6259903c1d8ba1d651efe010de80395
ryanchua00 <ryanchuahj@gmail.com>
2024-07-12 15:49:03 +0800

    show request body

--- roobot-serverless/app/api/pickapoods/route.ts
+++ roobot-serverless/app/api/pickapoods/route.ts
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
-  return new Response(`Hello, received ${request}`);
+  return new Response(`Hello, received ${request.body}`);
 }
