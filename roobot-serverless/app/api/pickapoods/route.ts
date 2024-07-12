a69a111b8dc8e89f19f672fd960a054aeab4f8be
ryanchua00 <ryanchuahj@gmail.com>
2024-07-12 15:51:09 +0800

    add post req

--- roobot-serverless/app/api/pickapoods/route.ts
+++ roobot-serverless/app/api/pickapoods/route.ts
@@ -1,6 +1,13 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
+    console.log("GET")
+    console.log(request);
+    return new Response(`Hello, received ${request.body}`);
+}
+
+export function POST(request: Request) {
+    console.log("POST")
     console.log(request);
     return new Response(`Hello, received ${request.body}`);
 }
